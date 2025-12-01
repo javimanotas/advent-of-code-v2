cffc22bad25240cd6042dd4e49e958efbf2176f4
Javier Manotas Ruiz <jmanotasruiz@gmail.com>
2025-12-01 16:01:59 +0100

    feat: implement solution for 2025 day 01

--- aoc_2025/src/bin/day01.rs
+++ aoc_2025/src/bin/day01.rs
@@ -1,9 +1,30 @@
 use aoc_utils::{input, parser::*};
 
-fn main() {
-    // let part1 = todo!();
-    // println!("{part1}");
+fn password(rotations: &[i32]) -> usize {
+    let mut start = 50;
+    let mut cnt = 0;
+    for i in rotations {
+        start = (start + i) % 100;
+        if start == 0 {
+            cnt += 1;
+        }
+    }
+    cnt
+}
 
-    // let part2 = todo!();
-    // println!("{part2}");
+fn main() {
+    let rotations = input::parse_lines(
+        prefix("R") >> number().map(|n| n as i32) | prefix("L") >> number().map(|n| -(n as i32)),
+    );
+
+    let part1 = password(&rotations);
+    println!("{part1}");
+
+    let rotations = rotations
+        .iter()
+        .flat_map(|x| vec![x.signum(); x.unsigned_abs() as usize])
+        .collect::<Vec<_>>();
+
+    let part2 = password(&rotations);
+    println!("{part2}");
 }
